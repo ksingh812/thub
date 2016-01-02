03c6881d220be4cb41ca7510fe70bf5b46393b15
ksingh812 <kuldeepghotra@gmail.com>
2016-01-02 14:11:18 -0800

    live changes

--- themes/tkthub-child/header.php
+++ themes/tkthub-child/header.php
@@ -18,7 +18,28 @@
 
 		<link rel="profile" href="http://gmpg.org/xfn/11">
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-		
+		<script>
+		  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+		  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+		  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+		  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	
+		  ga('create', 'UA-28896235-1', 'tickethub.co');
+		  ga('require', 'displayfeatures');
+		  ga('send', 'pageview');
+	  </script>
+	  <script type="application/ld+json">
+		{
+		   "@context": "http://schema.org",
+		   "@type": "WebSite",
+		   "url": "http://www.tickethub.co/",
+		   "potentialAction": {
+			 "@type": "SearchAction",
+			 "target": "http://tickets.tickethub.co/results-general?kwds={search_term_string}",
+			 "query-input": "required name=search_term_string"
+		   }
+		}
+		</script>
 		<!--wordpress head-->
 		<?php wp_head(); ?>
 	</head>
